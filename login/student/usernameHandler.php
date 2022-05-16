1b0c8b56fbddde9b488db26ae01a1fe9248db597
Tatwamasi Mishra <tatwamasi01@gmail.com>
2022-05-16 16:53:55 +0530

    added comments

--- login/student/usernameHandler.php
+++ login/student/usernameHandler.php
@@ -2,32 +2,26 @@
     session_start();
     require_once "../../configdb.php";
     
-    // echo 
-    //         "<script>
-    //             console.log('email: ".$emailid."');
-    //         </script>";
     // if session is set
     if(isset($_SESSION["emailid"])) {
+        
         $emailid = $_SESSION["emailid"];
+        
+        // ------------------ querying to the database ----------------------        
         $sql = "select sname from studentinfo where emailid = '".$emailid."';";
         $result = $connection->query($sql);
-        echo "<script>console.log('queried the sql and got result');</script>";
+        
+        // displaying the vendor name
         if($result->num_rows > 0) {
             $row = $result->fetch_assoc();
             $sname = $row["sname"];
-            echo "<script>console.log('".$sname."');</script>";
             echo "<script>
                 document.getElementById('userName').textContent = '".$sname."!';
             </script>";
-            // echo 
-            // "<script>
-            //     document.getElementById('username').textContent = ".$sname.";
-            //     console.log('successfully updated username');
-            // </script>";
         }
         else {
             echo "<script>
-                console.log('query yielded no resulting rows.');
+                console.log('Student not found in the database.');
             </script>";
         }
     }
